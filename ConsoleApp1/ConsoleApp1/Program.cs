dc8c4431d46ba6251b526f8b77d94dac6bac06c2
kitturavi <ravi.kit1602@gmail.com>
2021-12-08 04:38:58 +0530

    push new changes
    program to check whether entered number is positive or negative.

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num > 0)
+                Console.WriteLine($"{num} is a positve number");
+            else if(num < 0)
+                Console.WriteLine($"{num} is a negative number");
+            else
+                Console.WriteLine($"{num} is a zero");
+            Console.ReadLine();
+        }
+    }
+}
