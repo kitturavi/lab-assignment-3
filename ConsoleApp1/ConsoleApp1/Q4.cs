7913898fde938d5fbe88890c00b313d4912621bf
kitturavi <ravi.kit1602@gmail.com>
2021-12-08 05:25:26 +0530

    push new changes
    program to decide whether a number is positive or not using ternary operator

---
+++ ConsoleApp1/ConsoleApp1/Q4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Q4
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the number :");
+            int num = Convert.ToInt32(Console.ReadLine());
+            string result = num >= 0 ? "positive":"negative";
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
